95ceac12197607510b1599a3205136d6b7108239
Jorge Gomez <jorge.electron@gmail.com>
2023-06-09 23:25:56 -0700

    adds app css ad faveicon, cleans up the project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import React from 'react';
-import './App.css';
-
-import About from "./components/About";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
+import React from "react";
 
 export default function App() {
   return (
-    <main>
-      <About />
-      <Footer />
-      <Header />
-      <Projects />
-      <Skills />
-    </main>
+    <div>
+
+    </div>
   );
 }
-
