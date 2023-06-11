011892e16398ecf3683a87070f4b3acce7e3a8f2
Jorge Gomez <jorge.electron@gmail.com>
2023-06-10 22:23:41 -0700

    adds animated links to hamburguer menu

--- src/screens/Navbar/Navbar.jsx
+++ src/screens/Navbar/Navbar.jsx
@@ -1,21 +1,51 @@
-import React from "react";
+import {useState, useEffect} from "react";
 import "./Navbar.css"
 import hamburgerBtn from "./hamburger-menu.svg"
 
 export default function Navbar() {
+    const [showLinks, setShowLinks] = useState (true);
+    const [showButton, setShowButton] = useState (false);
+
+    const handleClick = () => setShowLinks(!showLinks);
+
+    window.addEventListener("resize", (event) => {
+        if (event.target.innerWidth > 768){
+            setShowButton(false);
+            setShowLinks(true);
+        } else {
+            setShowButton(true);
+            setShowLinks(false);
+        }
+    })
+
+    useEffect(() => {
+        if (window.innerWidth > 768){
+            setShowButton(false);
+            setShowLinks(true);
+        } else {
+            setShowButton(true);
+            setShowLinks(false);
+        }
+    }, [])
+
     return(
         <div className = 'navbar-container'>
             <div className = 'navbar-name'> 
                 <div> Jorge Gomez </div>
-                <img alt='hamburguer menu' src={hamburgerBtn}/>
+                {showButton && (
+                    <img alt='hamburguer menu' src=
+                    {hamburgerBtn} onClick={handleClick}/>
+                )}
             </div>
-            <div className = 'navbar-links-container'>
-                <a href='#home'> Home </a>
-                <a href='#aboutme' > About Me </a>
-                <a href='#skills'> Skills </a>
-                <a href='#projects'> Projects </a>
-                <a href='#contact'> Contact </a>
-            </div>
+            {showLinks && (
+                <div className = 'navbar-links-container'>
+                    <a href='#home'> Home </a>
+                    <a href='#aboutme' > About Me </a>
+                    <a href='#skills'> Skills </a>
+                    <a href='#projects'> Projects </a>
+                    <a href='#contact'> Contact </a>
+                </div>
+            )}
         </div>
     )
 }
