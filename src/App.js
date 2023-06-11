30feebaeabab672b364e3b60c84239e46382a117
Jorge Gomez <jorge.electron@gmail.com>
2023-06-10 21:05:23 -0700

    adds hamburguer menu svg icon

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css"
 import { Navbar } from "./screens"
 
+
 export default function App() {
   return (
     <div>
--- src/screens/Navbar/Navbar.jsx
+++ src/screens/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Navbar.css"
+import hamburgerBtn from "./hamburger-menu.svg"
 
 export default function Navbar() {
     return(
         <div className = 'navbar-container'>
-            <div className = 'navbar-name'> Jorge Gomez </div>
+            <div className = 'navbar-name'> 
+                <div> Jorge Gomez </div>
+                <img alt='hamburguer menu' src={hamburgerBtn}/>
+            </div>
             <div className = 'navbar-links-container'>
                 <a href='#home'> Home </a>
                 <a href='#aboutme' > About Me </a>
