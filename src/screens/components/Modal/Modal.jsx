6104dc0135592ebd4169360071348fce040031e5
Jorge Gomez <jorge.electron@gmail.com>
2023-06-11 18:37:30 -0700

    adds a modal in Projects screen to show project's details and links

--- src/screens/components/Modal/Modal.jsx
+++ src/screens/components/Modal/Modal.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import "./Modal.css"
 
-function Modal() {
+
+function Modal({title, description, image, url}) {
     return (
-        <div>Modal</div>
+        <div className="modal-container">
+            <div className="modal-content">
+                <h2>{title}</h2>
+                    <button className="btn align-right">
+                        X
+                    </button>
+                    <img className="span2" src={require(`${image}`)} alt={title}/>
+                    <p className="span2">{description}</p>
+                    <a href={url} target="_blank" rel="noreferrer" >                    
+                    <button className="btn">
+                        Go to website
+                    </button>
+                    </a>
+            </div>
+        </div>
     )
 }
 
